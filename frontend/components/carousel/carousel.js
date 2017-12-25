e56eff4125f39d4702abf48f5c1a220c13340ec5
Arnaud de Surirey <adesurirey@gmail.com>
2017-12-25 19:48:34 +0100

    scaffold carousel banner

---
+++ frontend/components/carousel/carousel.js
@@ -0,0 +1,3 @@
+import "./carousel.css";
+import "./images/bacha-01.jpg";
+import "./images/bacha-02.jpg";
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,3 +1,9 @@
 import "init";
 import "components/page/page";
 import "components/navbar/navbar";
+import "components/carousel/carousel";
+import "components/banner/banner";
+
+import Rails from "rails-ujs";
+
+Rails.start();
