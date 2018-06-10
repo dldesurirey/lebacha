d094fc4cfa87e2a4540fa648d65bbceb698be6e1
Arnaud de Surirey <adesurirey@gmail.com>
2018-06-10 17:38:25 +0200

    rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module Lebacha
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.i18n.default_locale = :fr
 
